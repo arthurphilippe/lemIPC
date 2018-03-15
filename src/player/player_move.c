299bf93701d0c7dba93b9e945f13be67485f226c
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-15 23:01:49 +0100

    100% cov on the player moves

--- src/player/player_move.c
+++ src/player/player_move.c
@@ -5,6 +5,7 @@
 ** player_move
 */
 
+#include <stdio.h>
 #include "lemipc.h"
 
 /*
@@ -12,12 +13,12 @@
 */
 void player_move_towards(ipcs_t *ipcs, ivector_t *curr, ivector_t aim)
 {
-	ivector_t new_pos = {0, 0};
+	ivector_t new_pos = *curr;
 
 	if (curr->v_x != aim.v_x)
-		new_pos.v_x += (curr->v_x < aim.v_x) ? 1 : -1;
+		new_pos.v_x += ((curr->v_x < aim.v_x) ? 1 : -1);
 	if (curr->v_y != aim.v_y)
-		new_pos.v_y += (curr->v_y < aim.v_y) ? 1 : -1;
+		new_pos.v_y += ((curr->v_y < aim.v_y) ? 1 : -1);
 	if (shm_put_try(ipcs, new_pos, (char) ipcs->i_gpid + '0')) {
 		shm_put(ipcs, *curr, POS_EMPTY);
 		*curr = new_pos;
--- tests/test_player.c
+++ tests/test_player.c
@@ -42,4 +42,23 @@ Test(player, move_by, .init = ipc_delete_test, .fini = ipc_delete_test) {
 	cr_assert_eq(shm_pos_gpid_get(&ipcs, pos), 4);
 }
 
-Test(player, move_toward)
+Test(player, move_toward, .init = ipc_delete_test, .fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+
+}
