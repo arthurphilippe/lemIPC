82f06dd4b7aeda5842497ad089e39b7809dc5d25
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-19 13:45:51 +0100

    kappa

--- src/player/player_find_startpoint.c
+++ src/player/player_find_startpoint.c
@@ -18,13 +18,9 @@ ivector_t player_find_startpoint(ipcs_t *ipcs)
 			pos.v_y += 1;
 		}
 	} else if (ipcs->i_gpid == 2) {
-		pos = (ivector_t) {4, 1};
+		pos = (ivector_t) {1, 19};
 		while (!player_move_to(ipcs, &pos, pos)) {
-			pos.v_y += 1;
-			if (pos.v_y > BOARD_SIDE) {
-				pos.v_y = 0;
-				pos.v_x += 1;
-			}
+			pos.v_x += 1;
 		}
 	} else {
 		printf("stpt 3\n");
