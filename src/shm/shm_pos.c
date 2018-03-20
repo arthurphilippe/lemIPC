6bf419b54231feaa29e20f5416a933378f3bc99b
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-20 15:29:55 +0100

    fixed

--- src/shm/shm_pos.c
+++ src/shm/shm_pos.c
@@ -26,6 +26,9 @@ int shm_pos_gpid_get(ipcs_t *ipcs, ivector_t pos)
 {
 	size_t idx = pos.v_x * (BOARD_SIZE / BOARD_SIDE) + pos.v_y;
 
+	if (pos.v_x < 0 || pos.v_y < 0
+		|| pos.v_x >= BOARD_SIDE || pos.v_y >= BOARD_SIDE)
+		return (0);
 	if (!shm_pos_is_free(ipcs, pos) && ipcs->i_shmsg[idx] != POS_EMPTY)
 		return (ipcs->i_shmsg[idx] - '0');
 	return (0);
