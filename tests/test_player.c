c1abc6a7268193068531c2092ffd457a8ce6deb7
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-21 13:25:15 +0100

    testing for moves

--- tests/test_player.c
+++ tests/test_player.c
@@ -62,6 +62,226 @@ Test(player, move_toward, .init = ipc_delete_test, .fini = ipc_delete_test) {
 	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
 }
 
+Test(player, move_toward_ublock_xa, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 3});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {4, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+}
+
+Test(player, move_toward_ublock_xm, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+}
+
+Test(player, move_toward_ublock_ym, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 2});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {2, 2});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 1}), 7, "got %d, %d", pos.v_x, pos.v_y);
+}
+
+Test(player, move_toward_ublock_ya, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 2});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 1});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {2, 2});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 1});
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 2});
+}
+
+Test(player, move_toward_ublock_yn, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 2});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 1});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {2, 2});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 1});
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 2});
+}
+
+Test(player, move_toward_ublock_xn, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {4, 3});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {2, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 1});
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 2});
+}
+
+Test(player, move_toward_ublock_nothing, .init = ipc_delete_test,
+	.fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 2}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 1});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 1}), 7);
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 2}), 7);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {2, 3});
+	player_move_to(&ipcs, &pos, (ivector_t) {4, 3});
+	pos.v_x = 3;
+	pos.v_y = 2;
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {2, 3}), 7, "got %d, %d", pos.v_x, pos.v_y);
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 1});
+	player_move_towards(&ipcs, &pos, (ivector_t) {3, 2});
+}
+
+Test(player, move_fromward, .init = ipc_delete_test, .fini = ipc_delete_test) {
+	ipcs_t ipcs;
+	ivector_t pos = {0, 0};
+
+	ipcs.i_key = key_get(FTOK_FILE_PATH);
+	ipcs.i_shmsg = shm_get_new(ipcs.i_key);
+	ipcs.i_gpid = 7;
+
+	player_move_to(&ipcs, &pos, (ivector_t) {3, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {3, 3}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {0, 0});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {4, 4}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {3, 5});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {5, 3}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {1, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {6, 3}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {6, 3});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {6, 3}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {1, 2});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {7, 4}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {1, 5});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {8, 3}), 7);
+	player_move_fromwards(&ipcs, &pos, (ivector_t) {10, 5});
+	cr_assert_eq(shm_pos_gpid_get(&ipcs, (ivector_t) {7, 2}), 7);
+}
+
+
 Test(player, place, .init = ipc_delete_test, .fini = ipc_delete_test) {
 	ipcs_t ipcs;
 
